d4d1aad92afed361233b48f1ce55af483fbf7fb7
dairin007 <yasiniki777@gmail.com>
2021-01-12 01:16:14 +0900

    Update app.py

--- app/app.py
+++ app/app.py
@@ -42,7 +42,7 @@ def dated_url_for(endpoint, **values):
 
 @app.before_request
 def before_request():
-    if not request.is_secure
+    if not request.is_secure():
         url = request.url.replace("http://", "https://", 1)
         code = 301
         return redirect(url, code=code)
